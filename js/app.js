8242dd602e73624ae7c319c54af338cbe17ca05a
Antony Grove <agrove86@gmail.com>
2014-07-18 00:34:37 -0500

    2nd
    Absolute temperatures added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 	
+	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +12,62 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+
+
+
+
+  	function newGame(){
+  		var secretNumber = Math.floor(Math.random() * 100 + 1); // generates random # 1-100
+	
+  		$('#guessButton').click(function(event){
+	  		event.preventDefault();
+			var userGuessNum = $('#userGuess').val();
+  			
+  			$('#guessList').append('<li>' + userGuessNum + '</li>'); // adds number to the guess history.
+			$('#userGuess').val(''); // resets text input
+
+			alert(secretNumber)
+
+			if(userGuessNum == secretNumber){
+				alert('YOU WIN!');
+			}
+  			
+  			else if((userGuessNum > secretNumber - 3) && (userGuessNum < secretNumber + 3)){
+  				alert('Ridiculously Sun Scorching Hot!');
+  			}
+
+  			else if((userGuessNum > secretNumber - 5) && (userGuessNum < secretNumber + 5)){
+  				alert('Very Hot');
+  			}
+
+  			else if((userGuessNum > secretNumber - 9) && (userGuessNum < secretNumber + 9)){
+  				alert('Hot.');
+  			}
+
+  			else if((userGuessNum > secretNumber - 15) && (userGuessNum < secretNumber + 15)){
+  				alert('warm');
+  				
+  			}
+
+  			else if((userGuessNum > secretNumber - 22) && (userGuessNum < secretNumber + 22)){
+  				alert('cold');
+  			}
+
+  			else if((userGuessNum > secretNumber - 35) && (userGuessNum < secretNumber + 35)){
+  				alert('Ice cold');
+  			}
+
+  			else {
+  				alert('Colder than a ring around a polar bears ass');
+  			}
+		});
+
+	}// new game function end
+
+
+newGame();
+
 
 });
 
